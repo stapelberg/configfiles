7383e313f368a57e60e11228e48b3512c8ff948e
Michael Stapelberg <michael@stapelberg.de>
2017-04-01 21:29:57 +0200

    emacs: tell ag to follow symlinks
    …so that searching in ~/.emacs.d/lisp works, for example.

--- emacs-custom.el
+++ emacs-custom.el
@@ -4,3 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(message-header-to ((t (:foreground "green" :weight normal)))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ag-arguments (quote ("--smart-case" "--stats" "--follow"))))
