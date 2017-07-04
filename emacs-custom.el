5dfa953cdb474a67a80197c996d9f80684e2b01c
Michael Stapelberg <michael@stapelberg.de>
2017-07-04 03:21:41 -0700

    emacs-custom: commit selected packages
    Use package-install-selected-packages to install them on a new computer.

--- emacs-custom.el
+++ emacs-custom.el
@@ -9,4 +9,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ag-arguments (quote ("--smart-case" "--stats" "--follow"))))
+ '(ag-arguments (quote ("--smart-case" "--stats" "--follow")))
+ '(package-selected-packages (quote (ag use-package ack smex))))
