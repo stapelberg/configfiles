1280c8d940af112811486c2d95c9458dbd762211
Michael Stapelberg <michael@stapelberg.de>
2017-07-16 04:31:04 +0200

    emacs-zkj-go: initialize compile-command to “go run”

--- emacs-zkj-go.el
+++ emacs-zkj-go.el
@@ -16,5 +16,8 @@
   ;; One tab will be displayed as 4 spaces.
   (set 'tab-width 4)
   ;; Enable eldoc (displays function signatures in the minibuf).
-  (go-eldoc-setup))
+  (go-eldoc-setup)
+  ;; Initialize compile-command to “go run”.
+  (set (make-local-variable 'compile-command)
+       (format "go run %s" (file-name-nondirectory buffer-file-name))))
 (add-hook 'go-mode-hook 'zkj-go-mode-hook)
