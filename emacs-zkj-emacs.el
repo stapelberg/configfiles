2fe25a7f5d7e7631e172b1d15336f05bb0a59bbe
Michael Stapelberg <michael@stapelberg.de>
2017-10-31 21:48:31 +0100

    emacs: always ask for y/n, never yes/no

--- emacs-zkj-emacs.el
+++ emacs-zkj-emacs.el
@@ -194,3 +194,6 @@ If you unset the urgency, you still have to visit the frame to make the urgency 
 ;; See editorconfig.org
 (if (require 'editorconfig nil t)
     (editorconfig-mode 1))
+
+;; Always ask for y/n, never yes/no.
+(defalias 'yes-or-no-p 'y-or-n-p)
