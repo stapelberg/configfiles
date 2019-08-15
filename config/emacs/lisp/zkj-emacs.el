7273d1e544ddf337237c37d78d732964887071ac
Michael Stapelberg <michael@stapelberg.de>
2019-08-15 19:52:37 +0200

    emacs: use https://github.com/sharkdp/fd with fzf (obeys .gitignore)

--- config/emacs/lisp/zkj-emacs.el
+++ config/emacs/lisp/zkj-emacs.el
@@ -352,6 +352,8 @@ If you unset the urgency, you still have to visit the frame to make the urgency 
 	 (daemons (seq-filter not-dot (directory-files socket-dir))))
     (mapcar (lambda (daemon) (server-eval-at daemon '(load-file user-init-file))) daemons)))
 
+(setq counsel-fzf-cmd "fdfind --type f | fzf -f \"%s\"")
+
 (defun fzf ()
   "fuzzy find on the closest git repository"
   (interactive)
