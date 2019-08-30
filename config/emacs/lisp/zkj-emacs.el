7599e6c4a27985e3ade4fd957c18d6ceb992e6d3
Michael Stapelberg <michael@stapelberg.de>
2019-08-30 20:35:06 +0200

    emacs: comment out editerconfig for now

--- config/emacs/lisp/zkj-emacs.el
+++ config/emacs/lisp/zkj-emacs.el
@@ -298,9 +298,9 @@ If you unset the urgency, you still have to visit the frame to make the urgency 
 ;; automatically revert buffers when files change
 (global-auto-revert-mode 1)
 
-;; See editorconfig.org
-(if (require 'editorconfig nil t)
-    (editorconfig-mode 1))
+;; ;; See editorconfig.org
+;; (if (require 'editorconfig nil t)
+;;     (editorconfig-mode 1))
 
 ;; Always ask for y/n, never yes/no.
 (defalias 'yes-or-no-p 'y-or-n-p)
