4251e3a51ac29d496b84aea2bbc5e8f0400e03d4
Michael Stapelberg <michael@stapelberg.de>
2020-05-23 11:34:54 +0200

    protobuf: build distri packages without colors in compilation mode

--- config/emacs/lisp/zkj-protobuf.el
+++ config/emacs/lisp/zkj-protobuf.el
@@ -1,12 +1,19 @@
 (provide 'zkj-protobuf)
 
+;; NOTE: (setq jit-lock-defer-time 0.01) makes the distri build of
+;; e.g. networkmanager hang.
+
 (add-to-list
  'auto-mode-alist
  '("\\.textproto\\'" .
    (lambda ()
      ;; Enable protobuf mode.
      (protobuf-mode)
-     ;; Pretend we’re using http://localhost/ instead of a file:// URI,
-     ;; and pretend asciidoc was HTML.
-     (setq-local compile-command "distri")
+     (setq-local compile-command "distri <&-")
+     ;; Compile without colors (faster):
+     (add-hook 'compilation-mode-hook
+	       (lambda()
+		 ;; TODO: scope setting to only this compilation, not all
+		 ;; compilations.
+		 (font-lock-mode -1)))
      )))
