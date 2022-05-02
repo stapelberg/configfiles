4fc19e71bfab8c161f04a7bc1f6fe17e1f1df60b
Michael Stapelberg <michael@stapelberg.de>
2022-05-02 09:32:54 +0200

    emacs-starts-up: clone into separate, writeable git repository

--- travis/emacs-starts-up.bash
+++ travis/emacs-starts-up.bash
@@ -2,13 +2,16 @@
 
 set -eux
 
+wd=$PWD
 ln -sf $HOME/configfiles/config/emacs .emacs.d
+git clone /root/configfiles configfiles.clone
+cd configfiles.clone
 # Explicitly load init.el because --batch implies -q.
 # Run magit-status, which triggers more delayed package
 # installation.
 emacs \
     --debug-init \
     --batch \
-    --eval='(load-file "$PWD/.emacs.d/init.el")' \
-    --eval='(magit-status)' \
+    --eval="(load-file \"$wd/.emacs.d/init.el\")" \
+    --eval="(magit-status)" \
     --kill
